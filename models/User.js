5b00f4d3868fa99ab01f5aa9c0b62dffdc04e882
Todd Stuke <tddstuke@gmail.com>
2022-06-04 18:01:25 -0500

    created routes and set up sequelize class

---
+++ models/User.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// create our User model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+  {
+    // TABLE COLUMN DEFINITIONS GO HERE
+    // define an id column
+    id: {
+      // use the special sequelize datatypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      // this is the equivalent of SQL'S not null option
+      allowNull: false,
+      // instruct that this is the PRIMARY KEY
+      primaryKey: true,
+      // turn on auto increment
+      autoIncrement: true,
+    },
+    // define a username column
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // define email column
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // there cannot be any dupicate email values in this table
+      unique: true,
+      // if allowNull is set to false, we can run our data through validators before creating the table data
+      validate: {
+        isEmail: true,
+      },
+    },
+    // define a password column
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // this means the password must be at least four characters long
+        len: [4],
+      },
+    },
+  },
+  {
+    // TABLE CONFIGURATION OPTIONS GO HERE
+
+    // pass in our imported sequelize connection (the direct connection to the db)
+    sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    // don't pluralize name of database table
+    freezeTableName: true,
+    // use underscores instead of camel-casing
+    underscored: true,
+    // make it so our model name stays lowercase in the database
+    modelName: "user",
+  }
+);
+
+module.exports = User;
