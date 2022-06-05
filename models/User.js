7c9c2ac74b243192c9f03c82c9d44b5ec30f64d2
Todd Stuke <tddstuke@gmail.com>
2022-06-05 13:31:44 -0500

    added bcrypt npm to hash passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
 
 // create our User model
 class User extends Model {}
@@ -46,6 +47,22 @@ User.init(
     },
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set kup beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+
     // TABLE CONFIGURATION OPTIONS GO HERE
 
     // pass in our imported sequelize connection (the direct connection to the db)
