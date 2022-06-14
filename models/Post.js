b5c187161adb6564ea7c4f9fa20449db525cf847
Todd Stuke <tddstuke@gmail.com>
2022-06-13 21:10:58 -0500

    bug

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,5 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
-const { truncate } = require("./User");
 
 // create Post model
 class Post extends Model {
